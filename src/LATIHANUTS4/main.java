4af9bbbecb7d0bccdc4e6b45cbc1785cacf061c3
USER <USER@DESKTOP-68B98IN>
2017-11-14 22:33:48 +0700

    Latihan Uts 4

---
+++ src/LATIHANUTS4/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LATIHANUTS4;
+
+/**
+ *
+ * @author USER
+ */
+public class main {
+    
+    public static void main(String[] args) { 
+    ModelDesigner obj0=new ModelDesigner();
+        obj0.bonusDesigner();
+        ModelProgrammer obj1=new ModelProgrammer();
+        obj1.bonusProgrammer();
+        ModelSystemAnalyst obj2=new ModelSystemAnalyst();
+        obj2.bonusSystemAnalyst();
+        
+    }
+        
+}
